e80c2b34ba32e07a15025951da7813d97e7c6994
Vladyslav Lopatynskyi <lopatinskiyvladislav@gmail.com>
2017-07-25 19:36:44 +0200

    pep8 styling and docs added

--- movie.py
+++ movie.py
@@ -1,7 +1,8 @@
 class Movie(object):
-	"""Movie class is used to describe your favorite movie. It contains title, poster_image_url and trailer_youtube_url properties"""
-	def __init__(self, title, poster_image_url, trailer_youtube_url):
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
-		
+    """Movie class is used to describe your favorite movie.
+    It contains title, poster_image_url and trailer_youtube_url properties"""
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """Class constructor to initialize a basic movie props"""
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
--- project1.py
+++ project1.py
@@ -1,19 +1,21 @@
 import movie
 import fresh_tomatoes
 
-movieList = []
 # Creating objects with different movies
-police_academy = movie.Movie("Fresh cadets need to protect citizens", 
-	"http://www.impawards.com/1984/posters/police_academy.jpg",
-	"https://www.youtube.com/watch?v=FebYWUGucpA")
+police_academy = movie.Movie(
+    "Fresh cadets need to protect citizens",
+    "http://www.impawards.com/1984/posters/police_academy.jpg",
+    "https://www.youtube.com/watch?v=FebYWUGucpA")
 
-matrix = movie.Movie("Everything you have heard before is a lie and this is a lie too",
-	"https://c1.staticflickr.com/9/8819/17072638696_f871731849_b.jpg",
-	"https://www.youtube.com/watch?v=m8e-FF8MsqU")
+matrix = movie.Movie(
+    "Everything you have heard before is a lie and this is a lie too",
+    "https://c1.staticflickr.com/9/8819/17072638696_f871731849_b.jpg",
+    "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
-fifth_element = movie.Movie("Dive into nearest future to get a sneak peak what to expect from it",
-	"http://img.moviepostershop.com/the-fifth-element-movie-poster-1997-1010473456.jpg",
-	"https://www.youtube.com/watch?v=199EvXTKucc")
+fifth_element = movie.Movie(
+    "Dive into nearest future to get a sneak peak what to expect from it",
+    "http://img.moviepostershop.com/the-fifth-element-movie-poster-1997-1010473456.jpg",
+    "https://www.youtube.com/watch?v=199EvXTKucc")
 
 # Generate html page with your favorite movies
 fresh_tomatoes.open_movies_page([police_academy, matrix, fifth_element])
